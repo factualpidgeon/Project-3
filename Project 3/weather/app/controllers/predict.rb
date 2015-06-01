59ca036e2c8cd920be53f604288bc16c94c54c0b
factualpidgeon <beau.j.day@gmail.com>
2015-06-02 09:53:37 +1000

    Update predict.rb
    Predict.rb is finished

--- Project 3/weather/app/controllers/predict.rb
+++ Project 3/weather/app/controllers/predict.rb
@@ -1,20 +1,29 @@
+require 'matrix'
+
 class Predict
 		
 	def initialize(x, y)
-		@x = x
-		@y = y
+		@xs = x
+		@ys = y
 	end
-	
-	def regress()
-	@a = 1
-	@b = 1
-	
+
+	def regres()
+		degree = 1
+		x_data = @xs.map { |x_i| (0..degree).map { |pow| (x_i**pow).to_f} }
+		mx = Matrix[*x_data]
+		my = Matrix.column_vector(@ys)
+		@coefficients = ((mx.t * mx).inv * mx.t * my).transpose.to_a[0]
 	end
 	
 	def gety(period)
 		predictions = []
-		0.step(period, 10) { |x| predictions<<(@a*x + @b) }
-		
+		0.step(period, 10).with_index do |x, i|  
+			y = 0
+			@coefficients.each_with_index do |a, n|
+				y += a * (x ** n)
+			end
+			predictions<<y
+		end
 		return predictions
 	end
 
