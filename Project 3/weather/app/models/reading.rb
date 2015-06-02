e31fdb9a6528675bd5ff7bb3e4ad6da370454af9
Paul Carr <paulvictorcarr@gmail.com>
2015-06-02 16:10:31 +1000

    Added current conditions
    Just updated readings with a cond method to return current 1 word
    conditions. (Very simple, but probably? accurate)

--- Project 3/weather/app/models/reading.rb
+++ Project 3/weather/app/models/reading.rb
@@ -5,4 +5,18 @@ class Reading < ActiveRecord::Base
     h = {time: self.timestamp.strftime("%H:%M:%S %p"), temp: self.temp, precip: self.rainfall, wind_direction: winddirection, wind_speed: windspeed}
     return h
   end
+
+  def cond
+    prev = self.day.order("created_at DESC").offset(3).first
+    if(prev.rainfall < self.rainfall)
+      if(self.temp < 0)
+        return "hailing"
+      end
+      return "raining"
+    end
+    if(self.temp < 20)
+      return "cloudy"
+    end
+    return "sunny"
+  end
 end
